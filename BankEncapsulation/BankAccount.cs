b4cbc7b3ff636067a119f22f054e1f527c59cb00
Jake Hammond <jakehammond226@gmail.com>
2022-07-14 15:14:06 -0500

    finished

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+            
+        }
+
+        private double balance = 0;
+
+        
+        public void Deposit(double inputdep)
+        {
+            balance += inputdep;
+        }
+
+        public double GetBalance()
+        {
+            return balance; 
+        }
+
+        public void Withdrawel(double inputwithdrawel)
+        {
+            balance -= inputwithdrawel;  
+        }
+
+    }
+}
+
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using BankEncapsulation;
+
+var BankAccount1 = new BankAccount();
+
+Console.WriteLine("Welcome to your bank account\n");
+
+
+Console.WriteLine("Would you like to withdraw money?\n");
+
+var withdrawInput = Console.ReadLine();
+
+if (withdrawInput == "yes")
+{
+    Console.WriteLine("How much do you want to withdraw?\n");
+
+    var withdrawAmount = Convert.ToInt32((Console.ReadLine()));
+
+
+    BankAccount1.Withdrawel(withdrawAmount);
+
+    Console.WriteLine($"Your account balance after your withdrawel is now {BankAccount1.GetBalance()}\n");
+}
+
+
+Console.WriteLine("Would you like to deposit money?\n");
+
+var depositInput = Console.ReadLine();
+
+    if (depositInput == "yes")
+    {
+        Console.WriteLine("How much money do you want to deposit?\n");
+
+        var depositAmount = Convert.ToInt32(Console.ReadLine());
+
+        BankAccount1.Deposit(depositAmount);
+
+        Console.WriteLine($"You account balance after your deposit is now {BankAccount1.GetBalance()}\n");
+    }
+
+
+
+Console.WriteLine($"Your balance is {BankAccount1.GetBalance()}");
+
+
+
+//Console.WriteLine("Would you like to deposit money?\n");
+
+
+
+
+
+//BankAccount1.Deposit();
+//BankAccount1.GetBalance(); 
